12c48a26b73adb75a34d1a6429ec588734a133f5
Tomaz <tomazgeddesdealmeida@tomazs-air.home>
2021-01-17 17:56:58 +0000

    first commit

---
+++ custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("aaa4c36ce00e572784d424554dcc9641c82d1155370770e231e10c649b59a074" "2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" default))
+ '(package-selected-packages '(org-bullets))
+ '(send-mail-function 'sendmail-send-it))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
